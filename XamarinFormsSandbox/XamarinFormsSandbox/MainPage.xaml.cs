b4006284a5399e44a32bdd61e5ea019e9fa35f10
Mafaza Suminta Putra <mafazasp@gmail.com>
2020-02-11 09:33:56 +0700

    Add project files.

---
+++ XamarinFormsSandbox/XamarinFormsSandbox/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFormsSandbox
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            slider.Value = 1;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Button Clicked!", "Press OK button to close this window...", "OK");
+        }
+
+    } 
+}
