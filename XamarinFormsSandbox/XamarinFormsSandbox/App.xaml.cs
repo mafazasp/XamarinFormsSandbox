b4a7c6a3cd48f61e34806f2e975577eb155393e2
Mafaza Suminta Putra <mafazasp@gmail.com>
2020-02-17 13:43:34 +0700

    First nav structure

--- XamarinFormsSandbox/XamarinFormsSandbox/App.xaml.cs
+++ XamarinFormsSandbox/XamarinFormsSandbox/App.xaml.cs
@@ -10,7 +10,7 @@ namespace XamarinFormsSandbox
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage (new HomePage());
         } 
 
         protected override void OnStart()
--- XamarinFormsSandbox/XamarinFormsSandbox/OpacitySlider.xaml.cs
+++ XamarinFormsSandbox/XamarinFormsSandbox/OpacitySlider.xaml.cs
@@ -11,9 +11,9 @@ namespace XamarinFormsSandbox
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class MainPage : ContentPage
+    public partial class OpacitySlider : ContentPage
     {
-        public MainPage()
+        public OpacitySlider()
         {
             InitializeComponent();
 
